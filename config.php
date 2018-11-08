dcce667a63656cf84b70e190e423ec6846d79273
JILL E NORBERG <35099642+jnorberg1550@users.noreply.github.com>
2018-11-07 22:35:28 -0800

    Troubleshooting 11/7

--- config.php
+++ config.php
@@ -9,15 +9,15 @@ Stores configuration data for our application
 
 //echo basename ($_SERVER['PHP_SELF']);
 
-define('THIS_PAGE', basename ($_SERVER['PHP_SELF']) );
+define('THIS_PAGE', basename ($_SERVER['PHP_SELF']));
 //echo'the constant is storing:' . THIS_PAGE;
 //die;
 
 //default page values
 $title = THIS_PAGE;
-$sitename = 'Site Name';
+$siteName = 'Site Name';
 $slogan = 'whatever';
-$pageHeader = 'Developer forgot to put in pageheader;
+$pageHeader = 'Developer forgot to put in pageheader';
 $subHeader = 'Developer forgot to put in subheader';
 
 switch(THIS_PAGE) {
@@ -32,19 +32,14 @@ switch(THIS_PAGE) {
         
     break;
         
-    case 'contact.php':
+    case 'contact.php';
         $title = 'My contact page';   
-	    $pageHeader = 'Please contact us':
-		$subHeader = 'we appreciate your feedback':
+	    $pageHeader = 'Please contact us';
+		$subHeader = 'We appreciate your feedback';
         
     break;
         
-    default:
-        $title = THIS_PAGE;
-		
-        
-        
-        
+      
         
 }
 
